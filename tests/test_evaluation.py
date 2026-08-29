9fa78b6a80e056971f173f5d9691380b94a9af67
Atmik Shetty <atmikshetty10@gmail.com>
2026-08-30 03:13:12 +0530

    chore: remove automated test checks

--- tests/test_evaluation.py
+++
@@ -1,7 +0,0 @@
-from manga_finetuning.evaluation import MONOCHROME, build_prompt
-
-
-def test_prompt_order_and_palette() -> None:
-    prompt = build_prompt("style_token", "solo, portrait")
-    assert prompt.index("style_token") < prompt.index(MONOCHROME) < prompt.index("solo")
-    assert MONOCHROME not in build_prompt("color_token", "solo", monochrome=False)
